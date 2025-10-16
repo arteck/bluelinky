7a3be961c4f73e0ad3873fce0cd46a53f7c8563e
arteck <6681528+arteck@users.noreply.github.com>
2025-10-16 18:08:32 +0200

    upd

--- dist/constants/australia.cfb.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-// Auto generated file on 2024-03-20T22:41:31.183Z
-// run `npm run eu:export:cfb` or `npm run au:export:cfb` respectively to update it
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.hyundaiCFB = exports.kiaCFB = void 0;
-exports.kiaCFB = Buffer.from('IDbMgWBXgic4MAyMgf5PFFRAdGX5O3IyC3uvN3scCs0gDpTFDuyvBorlAH9JMM2/hys=', 'base64');
-exports.hyundaiCFB = Buffer.from('V60WkEmyRQaAfrBF1623/7QL62MjLVbCHdItGzQ1g5T/hkmKmMVTaMHv4cKGzgD3kfc=', 'base64');
-//# sourceMappingURL=australia.cfb.js.map
